b29059310aaf511659f9150fac64b418035e2956
Harjot Singh <harjjotsinghh@gmail.com>
2023-08-23 11:13:17 +0530

    Installed some packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar.jsx';
+export const webAppName = "GoodName";
+export const themeColors = {
+  "text": "#FAFFFF",
+  "background": "#010A0C",
+  "primary": "#C42015",
+  "secondary": "#001013",
+  "accent": "#E13C1F"
+}
 
 function App() {
   return (
     <>
-      <div className="">
-        <h1>Hello there,</h1>
-        <h2>Welcome to Codivine!</h2>
-      </div>
+      <Navbar></Navbar>
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+
+  content: [
+    './pages/**/*.{html,js,jsx,tsx}',
+    './components/**/*.{html,js,jsx,tsx}',
+    "./src/**/*.{html,js,jsx,tsx}"
+  ],
   theme: {
     extend: {},
+    colors: {
+      "text": "#FAFFFF",
+      "background": "#010A0C",
+      "primary": "#C42015",
+      "secondary": "#001013",
+      "accent": "#E13C1F"
+    },
   },
   plugins: [],
 }
