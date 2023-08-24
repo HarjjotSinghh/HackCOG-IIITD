92105921942465bfda1243a6d2ff92e50cce41af
TheExaminedBaker <126285690+TheExaminedBaker@users.noreply.github.com>
2023-08-24 22:54:01 +0530

    home and find changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import logo from "../assets/img/android-chrome-192x192.png"
+import logo from "../assets/img/android-chrome-192x192.png";
 import { webAppName } from '../App';
 
 function Home() {
-    return (
-        <div className="bg-gray-100">
-            <div className="home-page text-center mt-48 mx-32">
-                <h1 className="text-6xl font-extrabold text-blue-800">{webAppName}</h1>
-                <h1 className="text-6xl absolute top-24 left-80 text-green-400 z-[-1] font-cursive">Welcome To</h1>
-            </div>
-
-            <section id="buttons" className="mt-20">
-                <div className="flex justify-center items-center space-x-24">
-                    <button type="button" className="btn btn-secondary px-4 py-2 bg-gray-500 text-lg font-bold text-white">Primary</button>
-                    <div className="border-2 border-green-500 h-24"></div>
-                    <button type="button" className="btn btn-secondary px-4 py-2 bg-gray-500 text-lg font-bold text-white">Primary2</button>
-                    <div className="connexusL absolute right-[-44%] z-[-1]">
-                        <img src={logo} alt="connexus logo" className="connexus-logo mt-24 w-[1300px] h-[1300px] z-[-1] animate-spin duration-10000 ease-in-out infinite"/>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className="bg-gray-100 h-screen flex flex-col justify-center items-center">
+      <div className="home-page text-center">
+        <h1 className="text-6xl absolute top-1/3 left-1/3 z-[-1] font-extrabold text-blue-800m -translate-x-1/2 -translate-y-1/2">Welcome To CONNEXUS</h1>
+      </div>
+      <section id="buttons" className="mt-20">
+        <div className="flex justify-center items-center space-x-8">
+          <button type="button" className="btn btn-secondary px-4 py-2 bg-gray-500 text-lg font-bold text-white">Primary</button>
+          <button type="button" className="btn btn-secondary px-4 py-2 bg-gray-500 text-lg font-bold text-white">Primary2</button>
+          <div className="border-2 border-green-500 h-24"></div>
         </div>
-    );
+      </section>
+
+      <div className="fixed top-0 right-0 h-screen w-[50%] bg-black overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          className="h-full w-full object-cover transform translate-x-[100%]"
+        >
+          <source src="path/to/your/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
 }
 
+
 export default Home;
+
